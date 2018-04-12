b60d613cac894adfd39363cbcc69e16335d1335d
Christian Hunstad <chhu@netlight.com>
2018-04-12 12:23:57 +0200

    Attempting to load npm modules in timertrigger

--- timerTrigger/index.js
+++ timerTrigger/index.js
@@ -1,21 +1,10 @@
-var requestPromise = require('request-promise');
+var Promise = require("bluebird");
+var requestPromise = require("request-promise");
+const reftokenAuth = require("../auth");
+var moment = require("moment-timezone");
+var azure = require("azure-storage");
+var tableService = azure.createTableService(process.env["AzureWebJobsStorage"]);
 
 module.exports = function(context, myTimer) {
-
-  var requestOptions = {
-    method: "GET",
-    json: true,
-    uri: encodeURI("https://" + process.env["appName"] + ".azurewebsites.net/api/documents_microapp")
-  };
-
-  return requestPromise(requestOptions)
-    .then((res) => {
-      context.log("Pinged documents with response: " + res);
-      context.done();
-    })
-    .catch((err) => {
-      context.log("Documents returned with: " + err);
-      context.done();
-    });
-
+  context.done();
 };
